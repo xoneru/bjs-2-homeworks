700004ce3ece4d73b76cfed719738e6eb998de1c
Michael Yareshchenko <xonezzer@gmail.com>
2022-04-07 16:26:53 +0500

    second version

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,10 @@
 function solveEquation(a, b, c) {
   "use strict"; 
   let arr;
-  let d = (b**2-4*a*c)
+  let d = (b ** 2-4*a*c)
   if (d < 0) {
     arr = [];
-  } else if (d = 0) {
+  } else if (d === 0) {
     let squareRoot = (-b/(2*a))
     arr = [squareRoot]
   } else {
@@ -15,27 +15,58 @@ function solveEquation(a, b, c) {
   return arr; // array
 }
 
+"use strict" // Строгий режим.
+function calculateMortgage() {
+    let percent = window.percent.value;
+    let contribution = window.contribution.value;
+    let amount = window.amount.value;
+    let date = window.date.value;
+
+    let result = calculateTotalMortgage(percent, contribution, amount, date);
+    let span = window.mortageResult;
+    span.textContent = result;
+}
+
+"use strict" // Строгий режим.
+function calculateMortgage() {
+    let percent = window.percent.value;
+    let contribution = window.contribution.value;
+    let amount = window.amount.value;
+    let date = window.date.value;
+
+    let result = calculateTotalMortgage(percent, contribution, amount, date);
+    let span = window.mortageResult;
+    span.textContent = result;
+}
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  // Контроль корректность введенных данных.    
-  if (percent > 0) {  
-   percent = +percent;
-  } else if (percent != 0 || percent == 0) {
-    return (`“Параметр "Процентная ставка" содержит неправильное значение ${percent}”.`);
+  let totalAmount;
+  let percentTest = Number(percent);
+  let contributionTest = Number(contribution);
+  let amountTest = Number(amount);
+  
+  if (percentTest != Number(percent)){
+	totalAmount = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+    return totalAmount	
+  } else if (contributionTest != Number(contribution)){
+	totalAmount = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+    return totalAmount  
+  } else if (amountTest != Number(amount)){
+	totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+    return totalAmount  
+	}
+	  else {
+  let credit = amount - contribution;
+  let dateMonth = date.getMonth();
+  let dateYear = date.getFullYear();
+  let dateNow = new Date(Date.now());
+  let dateNowMonth = dateNow.getMonth();
+  let dateNowYear = dateNow.getFullYear();
+  let month = (dateYear-dateNowYear)*12+(dateMonth-dateNowMonth);
+  
+  let payment = credit * (percent/12/100 + (percent/12/100 / (((1 + percent/12/100)**month - 1))));
+  totalAmount = payment * month;
+
+return Number(totalAmount.toFixed(2));
   }
-  if (contribution >= 0) {
-    contribution = +contribution;
-  } else if (contribution !=  0) {
-    return (`“Параметр "Начальный взнос" содержит неправильное значение ${contribution}”.`); 
-  }
-  if (amount >= 0) {
-    amount = +amount;
-  } else if (amount !=  0) {
-    return (`“Параметр "Общая стоимость" содержит неправильное значение ${amount}”.`);
-  }
-     let returnAmount = amount - contribution  // Сумма, которую необходимо вернуть банку. 
-     percent = percent / 1200; // Процентная ставка
-     let monthlyPay = returnAmount*(percent+percent/(((1+percent)**date)-1)); // Ежемесячная оплата
-     let  totalAmount = monthlyPay * date;  //  общая сумма, которую придется заплатить клиенту.
-     console.log(totalAmount.toFixed(2));
-     return totalAmount.toFixed(2); //  Возврат результата в функцию
-  }
+}
