a940e7b40daab5505f56353010feb2f911f70298
Michael Yareshchenko <xonezzer@gmail.com>
2022-04-11 15:47:12 +0500

    First version

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,9 +1,22 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+      sum += arr[i];
+    } else if (arr[i] < min) {
+      min = arr[i]
+      sum += arr[i]
+    } else {
+      sum += arr[i]
+    }
+    
+  }
+  let avg = +(sum / arr.length).toFixed(2);
   return { min: min, max: max, avg: avg };
 }
 
