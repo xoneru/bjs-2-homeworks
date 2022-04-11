0499fc9ae810f21d7a3e665741374b026a857916
Michael Yareshchenko <xonezzer@gmail.com>
2022-04-11 17:13:03 +0500

    third

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -5,15 +5,13 @@ function getArrayParams(arr) {
   let sum = 0;
   
   for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
     if (arr[i] > max) {
       max = arr[i];
-      sum += arr[i];
     } else if (arr[i] < min) {
-      min = arr[i]
-      sum += arr[i]
-    } else {
-      sum += arr[i]
+      min = arr[i];
     }
+    
     
   }
   let avg = +(sum / arr.length).toFixed(2);
@@ -21,24 +19,34 @@ function getArrayParams(arr) {
 }
 
 // Задание 2
+
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) sum += arr[i];
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {   
+    if (func(arrOfArr[i]) > max) max = func(arrOfArr[i]);
+  }  
   return max;
 }
 
+
 // Задание 3
-function worker2(arr) {
-  // Ваш код
-}
+  function worker2(arr) {
+    let min = Infinity;
+    let max = - Infinity;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i]
+      };
+      if (arr[i] < min) {
+        min = arr[i]
+      };
+    }
+    return Math.abs(max - min);
+  }
+
