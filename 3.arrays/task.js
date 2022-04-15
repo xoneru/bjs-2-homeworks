c64647248ac068f1560c93cbff20d9f4410bcc90
Michael Yareshchenko <xonezzer@gmail.com>
2022-04-15 13:12:05 +0500

    Second

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,14 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
-  return result; // boolean
+  result = arr1.every(function(item, index, arr1) {
+    return item === arr2[index] && arr1.length === arr2.length;
+  });
+  return result; // true or false
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter((item) => item > 0);
+  resultArr = resultArr.filter((item) => item % 3 === 0);
+  resultArr = resultArr.map((item) => item * 10);
   return resultArr; // array
 }
