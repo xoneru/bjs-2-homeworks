33bf466057b4baea01a4c5609c7cc5dad87c5e02
Michael Yareshchenko <xonezzer@gmail.com>
2022-04-18 14:52:42 +0500

    third

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,14 +1,12 @@
 function compareArrays(arr1, arr2) {
   let result;
   result = arr1.every(function(item, index, arr1) {
-    return item === arr2[index] && arr1.length === arr2.length;
+    return item === arr2[index] && arr1.length == arr2.length;
   });
-  return result; // true or false
+  return result; 
 }
 
 function advancedFilter(arr) {
-  let resultArr = arr.filter((item) => item > 0);
-  resultArr = resultArr.filter((item) => item % 3 === 0);
-  resultArr = resultArr.map((item) => item * 10);
+  let resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
   return resultArr; // array
 }
