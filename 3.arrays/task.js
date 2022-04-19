5b19ad682e992a1e70cc2021de78a9902198e6cb
Michael Yareshchenko <xonezzer@gmail.com>
2022-04-19 12:48:41 +0500

    Fourth

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,8 +1,5 @@
 function compareArrays(arr1, arr2) {
-  let result;
-  result = arr1.every(function(item, index, arr1) {
-    return item === arr2[index] && arr1.length == arr2.length;
-  });
+  let result = arr1.every((item, index, arr1) => item === arr2[index] && arr1.length === arr2.length);
   return result; 
 }
 
