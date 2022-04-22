b6c788c1d82e59572b6e204b56aaa8690fb29d21
Michael Yareshchenko <xonezzer@gmail.com>
2022-04-22 14:35:28 +0500

    Second

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,28 +1,43 @@
+"use strict"
 function Student(name, gender, age) {
   this.name = name;
   this.gender = gender;
-  this.age = age;
+  this.age = age;    
 }
 
-let studentMasha = new Student("Maria", "female", 22);
-let studentVika = new Student("Viktoria", "female", 19);
-let StudentMark = new Student("Mark", "male", 22);
-let studentAlexey = new Student("Alexey", "male", 27);
-let studentMisha = new Student("Mikhail", "male", 28);
+let studentMaria = new Student("Maria", "female", 22);
+let studentViktiria = new Student("Viktoria", "female", 24);
+let studentalexey = new Student("Alexey", "male", 22);
+let studentMark = new Student("Mark", "male", 19);
+let studentMikhail = new Student("Mikhail", "male", 27);
+
 Student.prototype.setSubject = function (subjectName) {
-  this.subject = subjectname;
+  this.subject = subjectName;
 }
 
 Student.prototype.addMark = function (mark) {
-  if(this.marks === undefined) {
-    this.mark = [];
-  }
-  this.mark.push(mark);
+    if(this.marks === undefined){ 
+      this.marks = [];
+    }
+      this.marks.push(mark);
 }
 
 Student.prototype.addMarks = function (...mark) {
-  if (this.marks === undefined) {
-    this.marks = [];
-  }
-  mark.forEach((item, idx, mark) => this.marks.push(mark[idx]))
+  if(this.marks === undefined){ 
+    this.marks = []; 
+    } 
+    mark.forEach((item, idx, mark) => this.marks.push(mark[idx]));   
+}
+
+Student.prototype.getAverage = function () { 
+  let sum = 0;
+  let marks = this.marks;
+  marks.forEach((item, idx, marks) => sum += item);
+  return sum / marks.length;
+}
+
+Student.prototype.exclude = function (reason) { 
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
