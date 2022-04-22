82f0a6240014d30493a9482547420fe9ca34b75a
Michael Yareshchenko <xonezzer@gmail.com>
2022-04-22 14:31:52 +0500

    First initiation

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,28 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
+let studentMasha = new Student("Maria", "female", 22);
+let studentVika = new Student("Viktoria", "female", 19);
+let StudentMark = new Student("Mark", "male", 22);
+let studentAlexey = new Student("Alexey", "male", 27);
+let studentMisha = new Student("Mikhail", "male", 28);
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectname;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined) {
+    this.mark = [];
+  }
+  this.mark.push(mark);
+}
+
+Student.prototype.addMarks = function (...mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+  }
+  mark.forEach((item, idx, mark) => this.marks.push(mark[idx]))
+}
