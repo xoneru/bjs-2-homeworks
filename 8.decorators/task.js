d6b23ff4094fe074deaa9b4399d0141ae8d2867c
Michael Yareshchenko <xonezzer@gmail.com>
2022-05-13 11:28:14 +0500

    First

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,51 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];  
+    function wrapper(...args) {
+      const hash = args.join(',');
+      let idx = cache.findIndex((item) => item.hash == hash);
+      if (idx !== -1) {
+        console.log("Из кэша: " + cache[idx].value);
+        return "Из кэша: " + cache[idx].value;
+      }
+  
+      let result = func(...args);
+      cache.push({
+        'hash': hash,
+        'value': result
+      });
+      if (cache.length > 5) {
+        cache.shift();
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+    return wrapper;
 }
 
 
 function debounceDecoratorNew(func) {
-  // Ваш код
+  let timer = null;  
+    function wrapper(...args) {
+      if (timer === null) {
+        func(...args);
+        clearTimeout(timer);
+        timer = setTimeout(() => func(...args), ms);
+      }
+    }
+    return wrapper;
 }
 
 function debounceDecorator2(func) {
-  // Ваш код
+  let timer = null;
+  
+    function wrapper(...args) {
+      if (timer === null) {
+        func(...args);
+        clearTimeout(timer);
+        timer = setTimeout(() => func(...args), ms);
+      }
+      wrapper.count++;
+    }
+    wrapper.count = 0;
+    return wrapper;
 }
